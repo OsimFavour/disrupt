05f57e906accaa3c8e5534fc3144fad1b2232916
Osim Favour <famos204@gmail.com>
2023-05-02 07:02:57 +0100

    Modify with Database

--- django_project/techblog/views.py
+++ django_project/techblog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        "author": "OsimFavour",
-        "title": "Tech Blog Post 1",
-        "content": "First Post Content",
-        "date_posted": "May 1, 2023"
-    },
-{
-        "author": "John Doe",
-        "title": "Tech Blog Post 2",
-        "content": "Second Post Content",
-        "date_posted": "May 1, 2023"
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "techblog/home.html", context)
 
