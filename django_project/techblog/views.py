85ef58ca5f62aea1e75cd12f8f4b2b054fbb0842
Osim Favour <famos204@gmail.com>
2023-05-06 17:13:59 +0100

    Update function based views to class based views

--- django_project/techblog/views.py
+++ django_project/techblog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -8,6 +10,55 @@ def home(request):
     }
     return render(request, "techblog/home.html", context)
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = "techblog/home.html"  # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+    paginate_by = 2
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ["title", "content"]
+
+    # Post Content by logged in author
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    # Posts should only be updated by Posts Author
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
 
 def about(request):
     return render(request, "techblog/about.html", {"title": "About"})
